6a7e33c67ac75ad65ac4d0e26ee4dcb82f8482b4
ThatColdToast <thatcoldtoast@gmail.com>
2021-11-22 22:43:57 -0600

    Multiple Objects Method 1 (Uniforms)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -48,7 +48,7 @@ int main(void)
 		/* Make the window's context current */
 		glfwMakeContextCurrent(window);
 
-		glfwSwapInterval(0); // Turn Vsync on
+		glfwSwapInterval(1); // Turn Vsync on
 	}
 
 	/* GLEW INIT */
@@ -61,10 +61,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			100.0f,  100.0f, 0.0f, 0.0f, // 0
-			200.0f,  100.0f, 1.0f, 0.0f, // 1
-			200.0f,  200.0f, 1.0f, 1.0f, // 2
-			100.0f,  200.0f, 0.0f, 1.0f  // 3
+			-50.0f, -50.0f, 0.0f, 0.0f, // 0
+			 50.0f, -50.0f, 1.0f, 0.0f, // 1
+			 50.0f,  50.0f, 1.0f, 1.0f, // 2
+			-50.0f,  50.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] =
@@ -91,14 +91,10 @@ int main(void)
 
 		// shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-		glm::mat4 mvp = proj * view * model;
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/dirt.png");
 		texture.Bind();
@@ -118,15 +114,16 @@ int main(void)
 		const char* glsl_version = "#version 330 core";
 		ImGui_ImplOpenGL3_Init(glsl_version);
 
+
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
+
 		// Our state
-		bool show_demo_window = true;
+		/*bool show_demo_window = true;
 		bool show_another_window = false;
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);*/
 
 		Renderer renderer;
-
-		float r = 0.0f;
-		float increment = 0.05f;
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
@@ -142,41 +139,30 @@ int main(void)
 			ImGui::NewFrame();
 
 			shader.Bind();
-
-			shader.SetUniformMat4f("u_MVP", mvp);
 			// shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
 
-			renderer.Draw(va, ib, shader);
+				renderer.Draw(va, ib, shader);
+			}
+
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.SetUniformMat4f("u_MVP", mvp);
+
+				renderer.Draw(va, ib, shader);
+			}
 
 			// GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = 0.05f;
-
-			r += increment;
-
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				ImGui::Separator();
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
+				ImGui::Begin("Hello, world!");
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
 
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::End();
